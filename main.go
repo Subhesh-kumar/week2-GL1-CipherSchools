8042a2f03caeb2e0581d995bae947f07a06fbb5c
Subhesh Kumar <82794288+Subhesh-kumar@users.noreply.github.com>
2023-02-04 23:57:41 +0530

    Update main.go

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"log"
 
-	"github.com/DigantaChauduri06/week2-GL1-CipherSchools/database"
-	"github.com/DigantaChauduri06/week2-GL1-CipherSchools/routers"
+	"github.com/Subhesh-kumar/week2-GL1-CipherSchools/database"
+	"github.com/Subhesh-kumar/week2-GL1-CipherSchools/routers"
 )
 
 func main() {
